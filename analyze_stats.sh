81fd2df436b4a195a22ddc6940f8a80886529d64
Will Russell <will.russell.01@gmail.com>
2022-11-24 13:34:36 -0500

    script fixes, now includes summary output for route and raw table for review with :set nowrap (vim) or -s (less)

--- analyze_stats.sh
+++ analyze_stats.sh
@@ -10,22 +10,15 @@ echo "what is the route you wish to look for? insert the be_backend route and pr
 read ROUTE
 
 
-DATAGATHERtesting() {
-	#new block testing
-	echo "backend details for this route"
-	echo "======================="
-	cat $CONFIG | grep $ROUTE
-}
-
-
 
 DATAGATHER() {
 ###########START DATAGATHER#########
 
+
 #LBTOT value count per pod:
 echo "lbtot values -- note that these are UNIQUE values and if cookies are enabled will not increment after initial hit for that client"
 echo "================================================================="
-for i in $(ls ./ | grep _clean.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE | awk '{print $2, $26}'; done
+for i in $(ls ./ | grep _cleaned.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE | awk '{print $2, $26}'; done
 echo "================================================================="
 
 echo "" 
@@ -33,7 +26,7 @@ echo ""
 #HTTP response values:
 echo "HTTP response values 1xx, 2xx, 3xx, 4xx, 5xx totals"
 echo "=================================================================="
-for i in $(ls ./ | grep _clean.out); do echo $i; cat $i | sed 's/|/ /' | awk '{print $2, $32, $33, $34, $35, $36}' | grep $ROUTE; done
+for i in $(ls ./ | grep _cleaned.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE | awk '{print $2, $32, $33, $34, $35, $36}' ; done
 echo "================================================================="
 
 echo "" 
@@ -41,19 +34,27 @@ echo ""
 #backend LB strategy:
 echo "LB strategy for this route"
 echo "=================================================================="
-for i in $(ls ./ | grep _clean.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE | grep BACKEND | awk {'print $50'}; done
+for i in $(ls ./ | grep _cleaned.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE | grep BACKEND | awk {'print $50'}; done
 echo "=================================================================="
-
 
 
 
 ##########end DATAGATHER#########
 }
 
+highlights_block (){
+	#this block dumps the raw haproxy.config route detail into a separate file for independant verification based on $ROUTE
+	cat $CONFIG | grep -A5 -B5 $ROUTE > config_${ROUTE}_highlight.out 
+
+	#gather raw summary bundle for verification
+	for i in $(ls ./ | grep _cleaned.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE >> rawstats_${ROUTE}_highlight.out
+
+}
 
 
 
 
 
 DATAGATHER
+highlights_block
 exit 0
