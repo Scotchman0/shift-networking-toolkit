c9505b0b93c7e4fcef86ceb337921a8e3164bc07
Will Russell <35974491+Scotchman0@users.noreply.github.com>
2022-11-25 09:27:00 -0500

    Update analyze_stats.sh
    update to syntax, readability + code comments, and refactoring to remove clunky code on route-type not needed since we look at haproxy.config directly

--- analyze_stats.sh
+++ analyze_stats.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 #haproxy stats parser
-#script to gather haproxy stats from each routerpod for analysis
+#script to gather haproxy stats from each routerpod for brief (most-common) analysis
 #provided as-is with no warranties for use in supporting Red Hat troubleshooting efforts
 
-
-echo "this script will summarize and export relevant haproxy stats for fast reference"
-
-echo "please specify which haproxy.config by inserting the filename below and pressing return"
+echo "this script will summarize and export relevant haproxy stats for fast reference: lbtot (hits) and http response values"
+echo "this script will run a lookup on every file with matching '_cleaned.out' filename in this folder, please run this script inside haproxy-gather/"
+echo ""
+echo "please specify which haproxy.config we need to examine by inserting the '<filename>_haproxy.config' below and pressing return"
 read CONFIG
 
-echo "what is the route you wish to look for? insert the be_backend route and press below - script will grep for this string"
+echo "what is the route you wish to look for? insert the route name and press return - script will grep for this string"
 read ROUTE
-
-
 
 DATAGATHER() {
 ###########START DATAGATHER#########
-
 
 #LBTOT value count per pod:
 echo "lbtot values -- note that these are UNIQUE values and if cookies are enabled will not increment after initial hit for that client"
@@ -28,19 +25,12 @@ echo ""
 
 #HTTP response values:
 echo "HTTP response values 1xx, 2xx, 3xx, 4xx, 5xx totals"
+echo "alignment is not always perfect here due to nature of stats gather, refer to generated ${ROUTE}_highlight.out for complete table.
 echo "=================================================================="
 for i in $(ls ./ | grep _cleaned.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE | awk '{print $2, $32, $33, $34, $35, $36}' ; done
 echo "================================================================="
 
 echo "" 
-
-#backend LB strategy:
-echo "LB strategy for this route"
-echo "=================================================================="
-for i in $(ls ./ | grep _cleaned.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE | grep BACKEND | grep -E 'random|roundrobin|leastconn|source'; done
-echo "=================================================================="
-
-
 
 ##########end DATAGATHER#########
 }
@@ -52,13 +42,15 @@ highlights_block () {
 	#gather raw summary bundle for verification
 	#set header first:
 	for i in $(ls ./ | grep _cleaned.out | head -n 1); do cat $i | head -n 1 > ${ROUTE}_highlight.out; done
+	#export the route string sets (total) into summary highlight.
 	for i in $(ls ./ | grep _cleaned.out); do echo $i; cat $i | sed 's/|/ /' | grep $ROUTE >> ${ROUTE}_highlight.out; done
+	
+	echo "filenames ${ROUTE}_highlight.out and config_${ROUTE}_highlight.out have been created for your convenience"
+	echo "open with less and type -S to chop lines, or with vim and use :set nowrap to view the route highlight block"
+	echo "config_*_highlight.out is the annotation highlight for this backend (what strategy is in use)"
+	echo "refer to https://access.redhat.com/solutions/6987555 for more information on analyzing this information"
 
 }
-
-
-
-
 
 DATAGATHER
 highlights_block
