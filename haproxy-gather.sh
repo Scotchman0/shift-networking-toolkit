de7b04feae31b527c5a38345db7b248961875c5e
Will Russell <will.russell.01@gmail.com>
2022-11-26 15:38:31 -0500

    patch updated to avoid grabbing failing pod haproxy.config as default, and also now only grabs 1 haproxy-config per deployment

--- haproxy-gather.sh
+++ haproxy-gather.sh
@@ -7,7 +7,9 @@
 cmd="echo 'show stat' | socat - UNIX-CONNECT:/var/lib/haproxy/run/haproxy.sock"
 info="echo 'show info' | socat - UNIX-CONNECT:/var/lib/haproxy/run/haproxy.sock"
 error="echo 'show error' | socat - UNIX-CONNECT:/var/lib/haproxy/run/haproxy.sock"
-default=$(oc get pods -n openshift-ingress | grep default | awk {'print $1'} | head -n 1)
+
+#define the first routerpod that is for default set and is also in Running status to avoid pulling a failing container's config
+default=$(oc get pods -n openshift-ingress | grep default | grep Running | awk {'print $1'} | head -n 1) 
 TARGETDIR=./haproxy-gather
 
 #create folder
@@ -37,8 +39,9 @@ for i in $(oc get pods -n openshift-ingress | grep router | awk {'print $1'}); d
 #gather haproxy.config to pair with output:
 oc cp ${default}:haproxy.config -n openshift-ingress ${TARGETDIR}/default_haproxy.config
 
-#gather haproxy.config from any other non-default router pods (probably going to get more than we want but better than not enough)
-for i in $(oc get pods -n openshift-ingress | grep router | grep -v default | awk {'print $1'}); do oc cp ${i}:haproxy.config -n openshift-ingress ${TARGETDIR}/${i}_haproxy.config; done
+#gather haproxy.config from any other non-default router pods
+#for i in $(oc get pods -n openshift-ingress | grep router | grep -v default | sort -u | awk {'print $1'}); do oc cp ${i}:haproxy.config -n openshift-ingress ${TARGETDIR}/${i}_haproxy.config; done
+for i in $(oc get deployment -n openshift-ingress | grep -v default | awk {'print $1'} | grep -v NAME); do a=$(oc get pod -n openshift-ingress | grep ${i} | awk {'print $1'} | head -n 1); oc cp ${a}:haproxy.config -n openshift-ingress $TARGETDIR/${i}_haproxy.config; done
 
 #tarball the contents
 tar czf haproxy-gather-$(date +"%Y-%m-%d-%H-%M-%S").tar.gz $TARGETDIR/
