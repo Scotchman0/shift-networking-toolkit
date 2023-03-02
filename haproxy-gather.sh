f2c86a431afd5df093a33d8265fe0700ea55879e
Will Russell <35974491+Scotchman0@users.noreply.github.com>
2023-03-02 13:34:10 -0500

    Update haproxy-gather.sh
    Improved naming handling to avoid multi-gather decompress all expanding to `haproxy-gather`, now creates unique filenames to avoid overlap. Also added thread-count gather + added some comments

--- haproxy-gather.sh
+++ haproxy-gather.sh
@@ -11,10 +11,12 @@ namespace="openshift-ingress" #If running on 3.11='default', 4.x='openshift-ingr
 selector="default" #If running on 3.11='router' #4.x='default'
 
 #define the first routerpod that is for default set and is also in Running status to avoid pulling a failing container's config
+#selector is necessary because 4.x supports shard instances which have prefix `router` and thus we avoid calling a shard haproxy.config by default.0
 default=$(oc get pods -n ${namespace} | grep ${selector} | grep Running | awk {'print $1'} | head -n 1) 
-TARGETDIR=./haproxy-gather
+DATESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+TARGETDIR=./haproxy-gather-${DATESTAMP}
 
-#create folder
+#create folder structure
 mkdir $TARGETDIR
 mkdir $TARGETDIR/raw_stats
 mkdir $TARGETDIR/haproxy_errors
@@ -23,14 +25,17 @@ mkdir $TARGETDIR/haproxy_info
 #grab pod overview:
 oc get pods -n ${namespace} -o wide > $TARGETDIR/pod_overview.out
 
-#gather raw stats output for each routerpod:
+#grab pod threadcount:
+for i in $(oc get po -n ${namespace} --no-headers | grep router | awk '{print $1}');do echo "Pod name: $i" &&  oc exec $i -- ps auuxx|grep haproxy|wc -l >> $TARGETDIR/pod_thread_count.out; done
+
+#gather raw stats output for each routerpod for this reload cycle:
 for i in $(oc get pods -n ${namespace} | grep router | awk {'print $1'}); do oc exec $i -n ${namespace} -- bash -c "$cmd" > ${TARGETDIR}/${i}_rawstats; done
 
 #clean the stats entries for readability:
-for i in $(ls ./haproxy-gather/ | grep _rawstats); do column -s, -t < ./haproxy-gather/${i} | less -#2 -N -S > ./haproxy-gather/${i}_cleaned.out; done
+for i in $(ls ${TARGETDIR} | grep _rawstats); do column -s, -t < ${TARGETDIR}/${i} | less -#2 -N -S > ${TARGETDIR}/${i}_cleaned.out; done
 
 #move the dirtystats to $TARGETDIR/raw_stats
-for i in $(ls ./haproxy-gather/ | grep "_rawstats$"); do mv ./haproxy-gather/${i} ./haproxy-gather/raw_stats/; done
+for i in $(ls ${TARGETDIR} | grep "_rawstats$"); do mv ${TARGETDIR}/${i} ${TARGETDIR}/raw_stats/; done
 
 #gather info tables
 for i in $(oc get pods -n ${namespace} | grep router | awk {'print $1'}); do oc exec $i -n ${namespace} -- bash -c "$info" > ${TARGETDIR}/haproxy_info/${i}_info.out; done
@@ -45,4 +50,4 @@ oc cp ${default}:haproxy.config -n ${namespace} ${TARGETDIR}/default_haproxy.con
 for i in $(oc get deployment -n ${namespace} | grep -v default | awk {'print $1'} | grep -v NAME); do a=$(oc get pod -n ${namespace} | grep ${i} | awk {'print $1'} | head -n 1); oc cp ${a}:haproxy.config -n ${namespace} $TARGETDIR/${i}_haproxy.config; done
 
 #tarball the contents
-tar czf haproxy-gather-$(date +"%Y-%m-%d-%H-%M-%S").tar.gz $TARGETDIR/
+tar czf haproxy-gather-${DATESTAMP}.tar.gz $TARGETDIR/
