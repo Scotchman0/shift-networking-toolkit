c819041372156db746eeea9e79511522d6ec864a
Will Russell <35974491+Scotchman0@users.noreply.github.com>
2024-01-04 15:02:24 -0500

    Create nc_socket_probe_until_error.sh

---
+++ nc_socket_probe_until_error.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# A basic script to loop a netcat against a target ip and port, log output and stop the loop once we encounter an unexpected result
+
+IP="$1"
+PORT="$2"
+DATE=$(date +"%Y-%m-%d-%H-%M")
+LOGPATH=${DATE}_nc_results.out
+
+nc_loop() {
+ while true; do
+   DATE=$(date +"%Y-%m-%d-%H-%M")
+   # Run netcat and capture its output
+   OUTPUT=$(nc -v -z -i 1 $IP $PORT 2>&1)
+
+   # Print the output
+   echo "$OUTPUT --> $DATE"
+   
+   # Check if the output contains "Connection refused"
+   if [[ $OUTPUT != *"Connected to"* ]]; then
+     echo "Connection error: $OUTPUT"
+     break
+   fi
+   
+   # Log the output
+   echo "$OUTPUT" >> $LOGPATH
+   sleep 1
+ done
+}
+
+nc_loop
