5c4ced21c822548b5c008279be79b80c629e3a37
Will Russell <will.russell.01@gmail.com>
2024-07-23 16:58:20 -0400

    modified tcpdump_specific_iface to include /dev/shm to ensure tmpfs selection on write

--- tcpdump_specific_iface.sh
+++ tcpdump_specific_iface.sh
@@ -15,9 +15,10 @@ start_tcpdump() {
 #options: -C 100 (100MB)
 #options: -W 5 (5 pcaps buffer)
 #options: -s 100 (snapshot length of 110 to truncate pcap size)
-  tcpdump -s 110 -i $IFACE1 -C 100 -W 5 -w /tmp/$IFACE1.pcap &
-  tcpdump -s 110 -i $IFACE2 -C 100 -W 5 -w /tmp/$IFACE2.pcap &
-  tcpdump -s 110 -i $IFACE3 -C 100 -W 5 -w /tmp/$IFACE3.pcap &
+#updated save path to /dev/shm to ensure tmpfs selection
+  tcpdump -s 110 -i $IFACE1 -C 100 -W 5 -w /dev/shm/$IFACE1.pcap &
+  tcpdump -s 110 -i $IFACE2 -C 100 -W 5 -w /dev/shm/$IFACE2.pcap &
+  tcpdump -s 110 -i $IFACE3 -C 100 -W 5 -w /dev/shm/$IFACE3.pcap &
 }
 
 stop_tcpdump() {
@@ -34,6 +35,6 @@ start_tcpdump
 echo "tcpdump is now running, press return to stop capture after you observe the flapping behavior"
 read dummyfile
 stop_tcpdump
-echo "files available at /tmp/, don't forget to move them to /host if running inside a toolbox pod/debug shell:"
-ls -l /tmp/ | grep ".pcap"
+echo "files available at /dev/shm/, don't forget to move them to /host/var/tmp if running inside a toolbox pod/debug shell:"
+ls -l /dev/shm/ | grep ".pcap"
 exit 0
