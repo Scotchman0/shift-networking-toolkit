4db8824c7ebdae1c3f1b05a9aabb01f4732b87ae
Will Russell <35974491+Scotchman0@users.noreply.github.com>
2024-10-02 10:22:57 -0400

    Update ipsec-gather.sh
    syntax update + removed PATH overwrite in favor of LOGPATH

--- OCPBUGS-41551/ipsec-gather.sh
+++ OCPBUGS-41551/ipsec-gather.sh
@@ -11,24 +11,24 @@ mkdir ./ipsec-sample-bundle
 
 for POD in $(oc -n openshift-ovn-kubernetes get pod -o wide | grep -E "$pod1|$pod2" | awk {'print $1'}); do
         echo "grabbing ip xrfm state from $POD"
-	oc -n openshift-ovn-kubernetes rsh $POD ip xfrm state > $LOGPATH/$POD_xrfm_state.out
+	oc -n openshift-ovn-kubernetes rsh pod/${POD} ip xfrm state > $LOGPATH/$POD_xrfm_state.out
         echo "grabbing ip xrfm policy from $POD"
-	oc -n openshift-ovn-kubernetes rsh $POD ip xfrm policy > $LOGPATH/$POD_xrfm_policy.out
+	oc -n openshift-ovn-kubernetes rsh pod/${POD} ip xfrm policy > $LOGPATH/$POD_xrfm_policy.out
         echo "grabbing ip xrfm monitor from $POD"
-	# oc -n openshift-ovn-kubernetes rsh $POD ip xfrm monitor > $LOGPATH/$POD_xrfm_monitor.out
+	# oc -n openshift-ovn-kubernetes rsh pod/${POD} ip xfrm monitor > $LOGPATH/$POD_xrfm_monitor.out
         echo "grabbing ipsec status from $POD"
-	oc -n openshift-ovn-kubernetes rsh $POD ipsec status > $LOGPATH/$POD_ipsec_status.out
+	oc -n openshift-ovn-kubernetes rsh pod/${POD} ipsec status > $LOGPATH/$POD_ipsec_status.out
         echo "grabbing ipsec.conf from $POD"
-	oc -n openshift-ovn-kubernetes rsh $POD cat /etc/ipsec.conf > $LOGPATH/$POD_ipsec.conf
+	oc -n openshift-ovn-kubernetes rsh pod/${POD} cat /etc/ipsec.conf > $LOGPATH/$POD_ipsec.conf
 	#ipsec.d/cno.conf and ipsec.d/openshift.conf will only be present on 4.15+ - null capture on 4.14 is okay
         echo "grabbing cno.conf from $POD"
-	oc -n openshift-ovn-kubernetes rsh $POD cat /etc/ipsec.d/cno.conf > $LOGPATH/$POD_cno.conf
+	oc -n openshift-ovn-kubernetes rsh pod/${POD} cat /etc/ipsec.d/cno.conf > $LOGPATH/$POD_cno.conf
         echo "grabbing openshift.conf from $POD"
-	oc -n openshift-ovn-kubernetes rsh $POD cat /etc/ipsec.d/openshift.conf > $LOGPATH/$POD_openshift.conf
+	oc -n openshift-ovn-kubernetes rsh pod/${POD} cat /etc/ipsec.d/openshift.conf > $LOGPATH/$POD_openshift.conf
         echo "creating log tarball from $POD"
-	oc -n openshift-ovn-kubernetes rsh $POD sh -c "tar -czf /tmp/libreswan.tar.gz /var/log/openvswitch/*"
+	oc -n openshift-ovn-kubernetes rsh pod/${POD} sh -c "tar -czf /tmp/libreswan.tar.gz /var/log/openvswitch/*"
         echo "grabbing log tarball from $POD"
-	oc -n openshift-ovn-kubernetes cp $POD:/tmp/libreswan.tar.gz $LOGPATH/$POD_libreswan.tar.gz
+	oc -n openshift-ovn-kubernetes cp pod/${POD}:/tmp/libreswan.tar.gz $LOGPATH/$POD_libreswan.tar.gz
 done
 echo "creating tarball from log sample"
 tar -czf ipsec-sample-bundle_${DATE}.tar.gz ./ipsec-sample-bundle/
