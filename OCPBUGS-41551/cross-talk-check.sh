e5c399d0f75e70ccbde080ee8d6b909276ee8349
Will Russell <will.russell.01@gmail.com>
2025-02-28 12:21:47 -0500

    adding cross-talk-check.sh for validating all pods with one another

---
+++ OCPBUGS-41551/cross-talk-check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#Will Russell
+#NOT a supported long/term deployment - provided as-is with no warranties for help in debugging/mitigating OVN-kube failure condition on OpenShift 4.14.30: 
+ 
+DATE=$(date +"%Y-%m-%d-%H-%M") 
+PROBELOG=${DATE}_healthprobe.out 
+ 
+healthprobe() { 
+##https://everything.curl.dev/cmdline/exitcode.html 
+## Operation timeout. The specified time-out period was reached according to the conditions. 
+##curl offers several timeouts, and this exit code tells one of those timeout limits were reached. 
+##Extend the timeout or try changing something else that allows curl to finish its operation faster. 
+##Often, this happens due to network and remote server situations that you cannot affect locally. 
+ 
+##iterate over all peer pods; get the exit result of a connection; if it's a timeout (28) then log the NAME of the NODE and the result timeout; otherwise skip + add a dot to progress log. 
+for SOURCE in $(oc get pod -n openshift-dns | awk {'print $1'}); do 
+	echo ${SOURCE}
+    for TARGET in $(oc get pod -n openshift-dns -o wide | grep dns-default | awk {'print $6'}); do RESULT=$(oc -n openshift-dns rsh $SOURCE curl --connect-timeout 1 -kv -s ${TARGET}:5353/health 2>&1 )
+      if [[ $? -eq 28 ]]
+      then 
+     	echo $TARGET 
+        echo $(oc -n openshift-dns get pod -o wide | grep $TARGET | awk {'print $7'}) Timed out
+      else dummyvalue=true; fi; echo "."; 
+    done
+done | tee $PROBELOG 
+}
+
+healthprobe
+echo "done"
+exit 0
