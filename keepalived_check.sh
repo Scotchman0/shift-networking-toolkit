2d2de9678c7515e06b99fea8e8533e5838147c70
Will Russell <will.russell.01@gmail.com>
2026-02-05 11:32:01 -0500

    overview update/added usage section, minor update

--- keepalived_check.sh
+++ keepalived_check.sh
@@ -4,15 +4,16 @@
 # Written by Will Russell, 2026 Red Hat
 # Provided AS-IS with no warranties of any kind, explicit or inferred, used only for diagnostics purposes
 
-#~# OVERVIEW #~#
+#~# USAGE #~#
+# ./keepalived_check.sh --help
 
+#~# OVERVIEW #~#
 # - Get current placement of keepalived VIP (who has ownership of VIP right now)
-# - Get historical placement of VIP and establish timeline of VIP changes (variable time output - default 12h)
-# - On live cluster, get (on demand) config files from keepalived pods
-# - On live cluster, get (on demand) log files from pods (runs manual scrape from node logs if inspect isn't sufficient)
-# - On live cluster, can (on demand) run limited live TCPdump on VRRP traffic + parse output to confirm flow
+# - Get historical placement of VIP and establish timeline of VIP changes
+# - On live cluster, get (on demand) config files from keepalived pods and network placement of said VIP from links
+# - On live cluster, get (on demand) log files from pods for review from keepalived project namespace
+# - On live cluster, get (on demand) run limited live TCPdump on VRRP traffic (5s) written to stdout to validate traffic flow
 # - For API VIP, check haproxy containers logs and check for fail codes
-# - Get Network config and validate node health with check for br-ex routing table + br-ex interface on OVNkube
 
 #~# VARIABLES #~#
 
